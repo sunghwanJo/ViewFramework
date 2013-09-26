b9b45e820791ce98d962f977afb7b088b5c66eb9
= <=>
2013-09-26 13:48:39 +0900

    delete

--- view/include/footer.php
+++
@@ -1,8 +0,0 @@
-<div class='block footer'>
-    <hr />
-    <p>
-        <div style="width:100%;text-align:center;font-family:'Nanum Gothic';font-weight:bold;margin-bottom:40px;color:#606060;">
-            COPYRIGHT ⓒ <span style='color:black;'>Sunhyoup</span>. All Rights Reserved. Design by <span style='color:black;'>Sunhyoup</span> Inc.
-        </div>
-    </p>
-</div>
